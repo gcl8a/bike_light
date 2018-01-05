c4c7348a5b83c5aced610d6a46be1ad86ed23d0c
GCL <lewin.gc@gmail.com>
2018-01-05 16:55:47 -0500

    Initial Commit

---
+++ bike_light/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bike_light
+//
+//  Created by Gregory C Lewin on 1/5/18.
+//  Copyright © 2018 Gregory C Lewin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
